a0abeb8fc58c5d5067679cfe8d3e5d29e018b8b9
fsociety <143713077+fsociety1001@users.noreply.github.com>
2023-10-08 11:27:11 -0300

    protoMAXCENTRAL.cpp

---
+++ lilypad<3/proto/protoMAXCENTRAL.cpp
@@ -0,0 +1,79 @@
+#define MAX30100_INT_STATUS     0x00  // Which interrupts are tripped
+#define MAX30100_INT_ENABLE     0x01  // Which interrupts are active
+#define MAX30100_FIFO_WR_PTR    0x02  // Where data is being written
+#define MAX30100_OVRFLOW_CTR    0x03  // Number of lost samples
+#define MAX30100_FIFO_RD_PTR    0x04  // Where to read from
+#define MAX30100_FIFO_DATA      0x05  // Ouput data buffer
+#define MAX30100_MODE_CONFIG    0x06  // Control register
+#define MAX30100_SPO2_CONFIG    0x07  // Oximetry settings
+#define MAX30100_LED_CONFIG     0x09  // Pulse width and power of LEDs
+#define MAX30100_TEMP_INTG      0x16  // Temperature value, whole number
+#define MAX30100_TEMP_FRAC      0x17  // Temperature value, fraction
+#define MAX30100_REV_ID         0xFE  // Part revision
+#define MAX30100_PART_ID        0xFF  // Part ID, normally 0x11
+
+#define MAX30100_ADDRESS        0x57  // 8bit address converted to 7bit
+
+typedef unsigned char uint8_t;
+typedef unsigned int uint16_t;
+typedef enum{
+  pw200,    // 200us
+  pw400,    // 400us 
+  pw800,    // 800us 
+  pw1600    // 1600us
+}pulseWidth;
+
+typedef enum{
+  sr50,    // 50 
+  sr100,   // 100
+  sr167,   // 167
+  sr200,   // 200 
+  sr400,   // 400
+  sr600,   // 600
+  sr800,   // 800 
+  sr1000   // 1000 
+}sampleRate;
+
+typedef enum{
+  i0,    // 0
+  i4,    // 4.4mA
+  i8,    // 7.6mA
+  i11,   // 11.0mA
+  i14,   // 14.2mA
+  i17,   // 17.4mA
+  i21,   // 20.8mA
+  i27,   // 27.1mA
+  i31,   // 30.6mA
+  i34,   // 33.8mA
+  i37,   // 37.0mA
+  i40,   // 40.2mA
+  i44,   // 43.6mA
+  i47,   // 46.8mA
+  i50    // 50.0mA
+}ledCurrent;
+
+class MAX30100 {
+public:
+  uint16_t IR = 0;     //data
+  uint16_t RED = 0;    
+
+  MAX30100();
+  void  setLEDs(pulseWidth pw, ledCurrent red, ledCurrent ir); 
+  void  setSPO2(sampleRate sr); 
+  int   getNumSamp(void);       
+  void  readSensor(void);      
+  void  shutdown(void);   
+  void  reset(void);      
+  void  startup(void);    
+  int   getRevID(void);   
+  int   getPartID(void);  //  ID
+  void  begin(pulseWidth pw = pw1600, // comprido
+              ledCurrent ir = i50,    // alto
+              sampleRate sr = sr100); // baixo
+  void  printRegisters(void);
+
+private:
+  void    I2CwriteByte(uint8_t address, uint8_t subAddress, uint8_t data);
+  uint8_t I2CreadByte(uint8_t address, uint8_t subAddress);
+  void    I2CreadBytes(uint8_t address, uint8_t subAddress, uint8_t * dest, uint8_t count);
+};
