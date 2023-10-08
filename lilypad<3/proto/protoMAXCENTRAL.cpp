65ca4de9d33001d87e3ccf29b08a9bd79dbacd8a
fsociety <143713077+fsociety1001@users.noreply.github.com>
2023-10-08 11:27:54 -0300

    protoMAXCENTRAL.cpp

--- lilypad<3/proto/protoMAXCENTRAL.cpp
+++ lilypad<3/proto/protoMAXCENTRAL.cpp
@@ -1,18 +1,18 @@
-#define MAX30100_INT_STATUS     0x00  // Which interrupts are tripped
-#define MAX30100_INT_ENABLE     0x01  // Which interrupts are active
-#define MAX30100_FIFO_WR_PTR    0x02  // Where data is being written
-#define MAX30100_OVRFLOW_CTR    0x03  // Number of lost samples
-#define MAX30100_FIFO_RD_PTR    0x04  // Where to read from
-#define MAX30100_FIFO_DATA      0x05  // Ouput data buffer
-#define MAX30100_MODE_CONFIG    0x06  // Control register
-#define MAX30100_SPO2_CONFIG    0x07  // Oximetry settings
-#define MAX30100_LED_CONFIG     0x09  // Pulse width and power of LEDs
-#define MAX30100_TEMP_INTG      0x16  // Temperature value, whole number
-#define MAX30100_TEMP_FRAC      0x17  // Temperature value, fraction
-#define MAX30100_REV_ID         0xFE  // Part revision
-#define MAX30100_PART_ID        0xFF  // Part ID, normally 0x11
+#define MAX30100_INT_STATUS     0x00  
+#define MAX30100_INT_ENABLE     0x01  
+#define MAX30100_FIFO_WR_PTR    0x02  
+#define MAX30100_OVRFLOW_CTR    0x03  
+#define MAX30100_FIFO_RD_PTR    0x04  
+#define MAX30100_FIFO_DATA      0x05  
+#define MAX30100_MODE_CONFIG    0x06  
+#define MAX30100_SPO2_CONFIG    0x07  // Oximetria
+#define MAX30100_LED_CONFIG     0x09  // Pulso
+#define MAX30100_TEMP_INTG      0x16  
+#define MAX30100_TEMP_FRAC      0x17  
+#define MAX30100_REV_ID         0xFE 
+#define MAX30100_PART_ID        0xFF  
 
-#define MAX30100_ADDRESS        0x57  // 8bit address converted to 7bit
+#define MAX30100_ADDRESS        0x57  
 
 typedef unsigned char uint8_t;
 typedef unsigned int uint16_t;
